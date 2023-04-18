c2b1e66fe8d7af167c2f5c1d7fc03c270878a5e1
Cristian Correa <cristian-correa@live.com>
2023-04-18 17:30:27 +0200

    Recibo custom data y muestro en pantalla (sin probar)

--- blufi_solo/blufi/components/BLUFI/blufi.c
+++ blufi_solo/blufi/components/BLUFI/blufi.c
@@ -434,8 +434,12 @@ static void example_event_callback(esp_blufi_cb_event_t event, esp_blufi_cb_para
         break;
     }
     case ESP_BLUFI_EVENT_RECV_CUSTOM_DATA:
-        BLUFI_INFO("Recv Custom Data %" PRIu32 "\n", param->custom_data.data_len);
-        esp_log_buffer_hex("Custom Data", param->custom_data.data, param->custom_data.data_len);
+        uint8_t cj_id;
+        //memset(cj_id, 0, param->custom_data.data_len);
+        memcpy(&cj_id, param->custom_data.data, param->custom_data.data_len); 
+        ESP_LOGI(TAG, "Recibido por custom data: %x", cj_id);  
+        //BLUFI_INFO("Recv Custom Data %" PRIu32 "\n", param->custom_data.data_len);
+        //esp_log_buffer_hex("Custom Data", param->custom_data.data, param->custom_data.data_len);
         break;
 	case ESP_BLUFI_EVENT_RECV_USERNAME:
         /* Not handle currently */
