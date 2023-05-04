e6c43a883043b4c28f58b8d42ca6f395050f53f6
PC Desk Windows <cristian-correa@live.com>
2023-05-04 22:22:53 +0200

    creo funciones para int en storaga

--- blufi_solo/blufi/components/STORAGE/storage.c
+++ blufi_solo/blufi/components/STORAGE/storage.c
@@ -14,10 +14,10 @@ int NVS_read(char *data, char *Get_Data) // data es la referencia
 {
     nvs_handle_t my_handle;
     esp_err_t err = nvs_open("storage", NVS_READWRITE, &my_handle);
-    int result = 1;
     if (err != ESP_OK)
     {
         printf("Error (%s) opening NVS handle!\n", esp_err_to_name(err));
+        return err; 
     } 
     else 
     {   
@@ -31,7 +31,6 @@ int NVS_read(char *data, char *Get_Data) // data es la referencia
                 printf("Done\n");
                 printf("Read data: %s\n", dato_leido);
                 strcpy(Get_Data, dato_leido);
-                result = 0; 
                 break;
             case ESP_ERR_NVS_NOT_FOUND:
                 printf("The value is not initialized yet!\n");
@@ -41,7 +40,7 @@ int NVS_read(char *data, char *Get_Data) // data es la referencia
         }
     }
     nvs_close(my_handle);
-    return result; 
+    return err; 
 }
 
 void NVS_write(char *data, char *write_string) // data es la referencia
@@ -66,28 +65,26 @@ void NVS_write(char *data, char *write_string) // data es la referencia
 int NVS_write_i8(char *name, int *datos)
 {
     nvs_handle_t my_handle;
-    int result = 1;
     esp_err_t err = nvs_open("storage", NVS_READWRITE, &my_handle);
     if(err != ESP_OK)
     {
-        return result; 
+        return err; 
     }else{
         err = nvs_set_i8(my_handle, name, datos);
         if(err != ESP_OK)
         {
             nvs_close(my_handle);
-            return result;
+            return err;
         }
         err = nvs_commit(my_handle);
         if(err != ESP_OK)
         {
             nvs_close(my_handle);
-            return result;
+            return err;
         }
     }
-    nvs_close(my_handle);
-    result = 0; 
-    return result;    
+    nvs_close(my_handle); 
+    return err;    
 }
 
 int NVS_read_i8(char *name, int *datos)
