99c5f74bb87e680e99ce7214da3426675cc1f5f9
Cristian Correa <cristian-correa@live.com>
2023-05-11 20:33:42 +0200

    elimino indice de F en header.c

--- blufi_solo/blufi/components/HEADER/header.c
+++ blufi_solo/blufi/components/HEADER/header.c
@@ -11,22 +11,9 @@ extern sensor_data mediciones;
 
 void recibe_confg_hum(char str[], config_data *cfg)
 {
-    /*
+
     int posH = strcspn(str, "H");
     int posL = strcspn(str, "L");
-    int posF = strcspn(str, "F");
-    char hum_H[posL-posH+1];
-    char hum_L[posF-posL+1];
-    memset(hum_H, 0, posL-posH+1);
-    memcpy(hum_H, str+posH+1, posL-posH);
-    memset(hum_L, 0, posF-posL+1);
-    memcpy(hum_L, str+posL+1, posF-posL);
-    cfg->hum_sup = atoi(hum_H);
-    cfg->hum_inf = atoi(hum_L);
-    */
-    int posH = strcspn(str, "H");
-    int posL = strcspn(str, "L");
-    int posF = strcspn(str, "F");
     char *hum_H = str + posH + 1;
     char *hum_L = str + posL + 1;
     cfg->hum_sup = strtol(hum_H, NULL, 10);
